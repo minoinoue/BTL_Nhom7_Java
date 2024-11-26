701406f607eb66e8c87ca0eeb08af1f9111bafc7
Mino Inoue <phungtuandat29052004@gmail.com>
2024-11-26 14:15:46 +0700

    Delete HeThongQLNV directory

--- HeThongQLNV/src/hethongqlnv/Main.java
+++
@@ -1,7 +0,0 @@
-package hethongqlnv;
-import javax.swing.*;
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
-    }
-}
